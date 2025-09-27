10eacc0694d1607e46008ee1b931dfaa0523abb6
Verumbra <128173230+Verumbra@users.noreply.github.com>
2025-09-26 22:18:29 -0500

    Refactor LevelingService property and constructors
    Renamed the XpXCurve property to XpCurve and updated its declaration. Began implementing overloaded constructors for LevelingService, including a default constructor that initializes XpCurve. Some code sections remain incomplete and require further implementation.

--- Services/LevelService/LevelingService.cs
+++ Services/LevelService/LevelingService.cs
@@ -2,20 +2,25 @@ namespace Rumba_Bot.Services.LevelService;
 
 public class LevelingService
 {
-    private string XpXCurve {get; set;}
-    private 
-    
+    private string XpCurve { get; set; }
+    private
+
     public LevelingService(var GuildXpCurve)
     {
         try
         {
-            
+
         }
         catch (Exception e)
         {
             //set to the default curve
         }
-        
+
+    }
+
+    public LevelingService()
+    {
+        XpCurve = 
     }
     
     
